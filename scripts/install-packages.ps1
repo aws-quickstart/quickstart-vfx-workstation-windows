b0120f94b55a992e2f33ca5b041ffa59f27bed48
Mark Stephens <mrkst@amazon.com>
2018-08-21 16:49:03 -0400

    Added retry downloads for the choclatey package manager

--- scripts/install-packages.ps1
+++ scripts/install-packages.ps1
@@ -8,9 +8,34 @@ try {
     #
     # Install packages
     #
+
+    # Download choclatey installer
     Write-Verbose "Install Chocolatey"
     $url = 'https://chocolatey.org/install.ps1'
-    iex ((new-object net.webclient).DownloadString($url))
+    $Source = "https://chocolatey.org/install.ps1"
+    $Destination = "c:\cfn\scripts\choclatey-install.ps1"
+    Write-Verbose "Trying to download Choclatey from $Source to $Destination"
+    $tries = 5
+    while ($tries -ge 1) {
+        try {
+            (New-Object System.Net.WebClient).DownloadFile($Source,$Destination)
+            break
+        }
+        catch {
+            $tries--
+            Write-Verbose "Exception:"
+            Write-Verbose "$_"
+            if ($tries -lt 1) {
+                throw $_
+            }
+            else {
+                Write-Verbose "Failed download. Retrying again in 5 seconds"
+                Start-Sleep 5
+            }
+        }
+    }
+    # Run choclatey installer
+    invoke-expression -Command $Destination
 
     echo "aws: Install 7zip"
     Write-Verbose "Install 7zip"
